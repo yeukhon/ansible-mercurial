206c9c0583af67f8bcbbd5bad151e52466860044
Yeukhon Wong <yeukhon@acm.org>
2013-01-17 14:00:11 -0500

    Closed #1 and #2. Renamed the module to hg. Changed rev to revision.

--- tests.py
+++ tests.py
@@ -1,15 +1,16 @@
 import os
-os.system('cd ansible && sudo make install && hacking/test-module -m /home/glasslab/ansible/library/mercurial -a "repo=https://bitbucket.org/yeukhon/scm-hook state=present dest=/home/glasslab/scm-hook"')
+import getpass
+os.system('cd ansible && sudo make install && hacking/test-module -m /home/$USER/ansible/library/hg -a "repo=https://bitbucket.org/yeukhon/scm-hook state=present dest=/home/$USER/scm-hook"')
 
-if os.path.exists("/home/glasslab/scm-hook"):
+if os.path.exists("/home/{user}/scm-hook".format(user=getpass.getuser())):
     print("scm-hook is cloned down.")
 else:
     raise Exception("scm-hook IS NOT CLONED DOWN")
-os.system("rm -rf /home/glasslab/scm-hook")
+os.system("rm -rf /home/{user}/scm-hook".format(user=getpass.getuser()))
 
 print("now tests on the server")
 os.system("ansible-playbook tests.yml -u vagrant -k")
 os.system("ansible-playbook tests.yml -u vagrant --tags 'bad_url'")
-os.system("ansible-playbook tests.yml -u vagrant --tags 'bad_rev'")
+os.system("ansible-playbook tests.yml -u vagrant --tags 'bad_revision'")
 
 #os.system("ansible/hacking/test-module -m
